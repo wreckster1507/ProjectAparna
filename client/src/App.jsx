bb91d6b9f78ca3fc46cdb5775015b08df13f503d
Varun Harinath <varunharinath15@gmail.com>
2024-03-31 11:57:30 +0530

    Add routes for About Us, Contact, Documents, Happenings, Facilities, and Classifieds

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home Route */}
         <Route path="/" element={<div>Home</div>} />
+        {/* About Us Routes */}
         <Route
           path="/aboutus/AboutSociety"
           element={<div>About societry</div>}
@@ -19,6 +21,34 @@ const App = () => {
           path="/aboutus/BlockRepresentaives"
           element={<div>Block RepresentaƟves</div>}
         />
+        {/* Contact Routes */}
+        <Route path="/contact/drectory" element={<div>Directory</div>} />
+        <Route path="/contact/LocationMap" element={<div>Location Map</div>} />
+        {/* Doucments Routes */}
+        <Route path="/doucments/bye-laws" element={<div>bye-laws</div>} />
+        <Route path="/doucments/SOP" element={<div>SOP</div>} />
+        <Route
+          path="/doucments/Rule&Guidelines"
+          element={<div>Rules & Guidelines</div>}
+        />
+        <Route
+          path="/doucments/new-to-society"
+          element={<div>New to society</div>}
+        />
+        <Route path="/doucments/downloads" element={<div>downloads</div>} />
+        {/* Happenings  Route*/}
+        <Route
+          path="/happenings/new-and-circulars"
+          element={<div>news and circilars</div>}
+        />
+        <Route
+          path="/happenings/photo-gallery"
+          element={<div>photos galaries</div>}
+        />
+        {/* Facilites Route */}
+        <Route path="facilites" element={<div>facilites</div>} />
+        {/* Classifieds Route */}
+        <Route path="/classifieds" element={<div>classifieds</div>} />
       </Routes>
     </BrowserRouter>
   );
